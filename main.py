0c3c65fe00949493b32aa0f2c0a21d78c449ceec
tzinchy <dreevxq@gmail.com>
2024-05-04 15:24:31 +0300

    new

--- main.py
+++ main.py
@@ -1,131 +1,54 @@
 import flet as ft
 import mysql.connector
 
-class User:
-    def __init__(self, use, passw):
-        self.user = use
-        self.password = passw
-
-    @property
-    def user(self):
-        return self.__user
-
-    @user.setter
-    def user(self, use):
-        self.__user = use
-
-    @property
-    def password(self):
-        return self.__password
-
-    @password.setter
-    def password(self, passw):
-        self.__password = passw
-
-#connection from mysql to py
-db_connection = mysql.connector.connect(
+user = mysql.connector.connect(
     user='root',
     password='',
     host='localhost',
     database='log'
 )
-conn = db_connection.cursor()
-def main_page(page):
-    def quer(e):
-        main(page)
-        page.update()
+
+def registration_user(page: ft.Page):
     page.clean()
+    page.theme_mode = 'dark'
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center'
+    #функция для перехода в окно логина
+    def to_log_window(e):
+        loging_user(page)
+    #добавляем все необходимые объекты для регистрации пользователя
+    page.add(ft.SafeArea(ft.Row([ft.Text("СТРАНИЦА С ВХОДОМ")], alignment=ft.MainAxisAlignment.CENTER)))
+    page.add(ft.SafeArea(ft.Row([ft.TextButton('Для прехода на логирование', on_click=to_log_window)], alignment=ft.MainAxisAlignment.CENTER)))
     page.update()
-    page.add(ft.Text("РАБОТАЕТ"))
-    page.add(ft.TextButton('Нажми и верни все', on_click=quer))
+
+def loging_user(page: ft.Page):
+    page.clean()
+    page.theme_mode = 'dark'
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center'
+    #функция для перехода в окно регистрации
+    def to_registration_window(e):
+        registration_user(page)
+    #добавляем все необходимые объекты на страницу с входом в аккаунт
+    page.add(ft.SafeArea(ft.Row([ft.Text("СТРАНИЦА С РЕГИСТРАЦИЕЙ")], alignment=ft.MainAxisAlignment.CENTER)))
+    page.add(ft.SafeArea(ft.Row([ft.TextButton('Для прехода на логирование', on_click=to_registration_window)], alignment=ft.MainAxisAlignment.CENTER)))
     page.update()
-#created window
-def main(page: ft.Page):
-    def start():
-        page.clean()
-        page.title = "loggs app"
-        page.theme_mode = 'dark'
-        page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-        def close_banner(e):
-            page.banner.open = False
-            page.update()
 
-        eror_login = ft.Banner(
-            bgcolor=ft.colors.RED,
-            leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.BLUE_ACCENT, size=40),
-            content=ft.Text(
-                "Пользователь не найден"
-            ),
-            actions=[
-                ft.TextButton("Попробуйте снова", on_click=close_banner),
-            ],
-        )
-        eror_password = ft.Banner(
-            bgcolor=ft.colors.RED,
-            leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.BLUE_ACCENT, size=40),
-            content=ft.Text(
-                "Ошибка в пароле"
-            ),
-            actions=[
-                ft.TextButton("Попробуй снова", on_click=close_banner),
-            ],
-        )
+def chooser(page: ft.Page):
+    page.clean()
+    page.theme_mode = 'dark'
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center'
+    def to_loging_user(e):
+        loging_user(page)
+    def to_registrate_user(e):
+        registration_user(page)
+    #переменные для действитя пользователя
+    to_reg = ft.TextButton('Зарегестрироваться', on_click=to_loging_user)
+    to_log = ft.TextButton('Войти',on_click=to_registrate_user)
+    page.add(ft.SafeArea(ft.Row([to_reg, to_log], alignment=ft.MainAxisAlignment.CENTER)))
+    page.update()
 
-        def show_error_login():
-            page.dialog = eror_login
-            eror_login.open = True
-            page.update()
-
-        def show_error_password():
-            page.dialog = eror_password
-            eror_password.open = True
-            page.update()
-
-        def show_welcome(user1):
-            dlg = ft.AlertDialog(
-                title=ft.Text(f'ДОБРО ПОЖАЛОВАТЬ\nLOGIN {user1.user}\nPASSWORD {user1.password}'),
-                on_dismiss=lambda e: print("Dialog dismissed!")
-            )
-            page.dialog = dlg
-            dlg.open = True
-            page.update()
-            db_connection.commit()
-
-        # Словарь соответствия сообщений и функций
-        message_actions = {
-            'Login not found.': show_error_login,
-            'Incorrect password provided.': show_error_password
-        }
-
-        def done(e):
-            main_page(page)
-            page.update()
-
-        def checker(e):
-            user1 = User(log.value, password.value)
-            try:
-                conn.callproc('AuthenticateUser', [user1.user, user1.password])
-                result = next(conn.stored_results())
-                message = result.fetchall()[0][0]
-
-                # Вызываем соответствующую функцию из словаря или показываем приветствие, если сообщение не найдено
-                action = message_actions.get(message, lambda: show_welcome(user1))
-                action()
-
-            except Exception as ex:
-                print('Error:', ex)
-
-        log = ft.TextField(label="login", width=200)
-        password = ft.TextField(label='password', width=200, can_reveal_password=True, password=True)
-        enter = ft.TextButton("Go to", width=400, height=50, on_click=checker)
-        page.add(ft.SafeArea(ft.Row(
-            [ft.Text("Введите данные для входа в учетную запись", size=15, weight=ft.FontWeight.W_500, selectable=True)],
-            alignment=ft.MainAxisAlignment.CENTER)))
-        page.add(ft.SafeArea(ft.Row([log, password], alignment=ft.MainAxisAlignment.CENTER)))
-        page.add(ft.SafeArea(ft.Row([enter], alignment=ft.MainAxisAlignment.CENTER)))
-        page.add(ft.TextButton('НАЖМИНА НА МЕНЯ', on_click=done))
-        page.update()
-    start()
-if __name__ == "__main__":
-    ft.app(target=main_page, view=ft.WEB_BROWSER)
+if __name__ == '__main__':
+    ft.app(chooser)
